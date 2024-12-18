a6a9e944bab446689755cf08aa07d52152764745
Daniel Stevens <daniel@dmskaspr.com>
2024-12-17 20:02:31 -0600

    Update browser.sh

--- scripts/browser.sh
+++ scripts/browser.sh
@@ -2,7 +2,7 @@
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
 
 chromium-browser \
-  $(jq -r '.site' ~/homehub/config/settings.json) \
+  $(jq -r '.site' /home/pi/homehub/config/settings.json) \
   --kiosk \
   --fast \
   --fast-start \
